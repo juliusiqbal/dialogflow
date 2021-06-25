25fb1883b7e81118728f98033ca336f4bce17050
Julius Iqbal <julius.jsr@gmail.com>
2021-06-26 04:43:02 +0600

    main: Added dialogflow messenger.

--- src/app.service.ts
+++ src/app.service.ts
@@ -5,8 +5,27 @@ import { HttpService } from '@nestjs/common/http';
 export class AppService {
   constructor(private readonly httpService: HttpService) {}
 
-  getHello(): string {
-    return 'Hello World!';
+  getWelcome(): string {
+    return `
+      <!DOCTYPE html>
+      <html lang="en">
+        <head>
+            <meta charset="UTF-8" />
+            <meta content="IE=edge" http-equiv="X-UA-Compatible" />
+            <meta content="width=device-width, initial-scale=1.0" name="viewport" />
+            <title>WeatherBot</title>
+            <script src="https://www.gstatic.com/dialogflow-console/fast/messenger/bootstrap.js?v=1"></script>
+        </head>
+        <body>
+          <df-messenger
+            agent-id="1a2e2c1f-6b36-46d5-91ea-92b5adfddd5d"
+            chat-title="WeatherBot"
+            intent="WELCOME"
+            language-code="en"
+          ></df-messenger>
+        </body>
+      </html>
+    `;
   }
 
   getLocalWeather(city: string): Promise<any> {
